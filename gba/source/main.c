6580cdeec5ca863d65731ca1fac9e3f0d642e4b2
zaksabeast <19464008+zaksabeast@users.noreply.github.com>
2023-10-12 06:37:52 -0500

    Rewrite GBA app

--- gba/source/main.c
+++ gba/source/main.c
@@ -5,45 +5,121 @@
  * of the MIT license.  See the LICENSE file for details.
  */
 #include <gba.h>
+#include <xcomms.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include "libSave.h"
 
-#define	REG_WAITCNT *(vu16 *)(REG_BASE + 0x204)
+#define REG_WAITCNT *(vu16 *)(REG_BASE + 0x204)
 #define JOY_WRITE 2
 #define JOY_READ 4
 #define JOY_RW 6
+#define MAX_SAVE_SIZE 0x20000
+#define MAX_BUF_SIZE 0x64
 
-u8 save_data[0x20000] __attribute__ ((section (".sbss")));
+u32 xcomms_recv();
+
+u8 save_data[MAX_SAVE_SIZE] __attribute__((section(".sbss")));
+
+void backup_save(u8 *out, u32 size)
+{
+	// disable interrupts
+	u32 prevIrqMask = REG_IME;
+	REG_IME = 0;
+	// backup save
+	switch (size)
+	{
+	case 0x200:
+		GetSave_EEPROM_512B(out);
+		break;
+	case 0x2000:
+		GetSave_EEPROM_8KB(out);
+		break;
+	case 0x8000:
+		GetSave_SRAM_32KB(out);
+		break;
+	case 0x10000:
+		GetSave_FLASH_64KB(out);
+		break;
+	case 0x20000:
+		GetSave_FLASH_128KB(out);
+		break;
+	default:
+		break;
+	}
+	// restore interrupts
+	REG_IME = prevIrqMask;
+}
+
+void write_save(u8 *save, u32 size)
+{
+	u32 prevIrqMask = REG_IME;
+	REG_IME = 0;
+	// write it
+	switch (size)
+	{
+	case 0x200:
+		PutSave_EEPROM_512B(save);
+		break;
+	case 0x2000:
+		PutSave_EEPROM_8KB(save);
+		break;
+	case 0x8000:
+		PutSave_SRAM_32KB(save);
+		break;
+	case 0x10000:
+		PutSave_FLASH_64KB(save);
+		break;
+	case 0x20000:
+		PutSave_FLASH_128KB(save);
+		break;
+	default:
+		break;
+	}
+	// restore interrupts
+	REG_IME = prevIrqMask;
+}
 
 s32 getGameSize(void)
 {
-	if(*(vu32*)(0x08000004) != 0x51AEFF24)
+	if (*(vu32 *)(0x08000004) != 0x51AEFF24)
 		return -1;
 	s32 i;
-	for(i = (1<<20); i < (1<<25); i<<=1)
+	for (i = (1 << 20); i < (1 << 25); i <<= 1)
 	{
-		vu16 *rompos = (vu16*)(0x08000000+i);
+		vu16 *rompos = (vu16 *)(0x08000000 + i);
 		int j;
 		bool romend = true;
-		for(j = 0; j < 0x1000; j++)
+		for (j = 0; j < 0x1000; j++)
 		{
-			if(rompos[j] != j)
+			if (rompos[j] != j)
 			{
 				romend = false;
 				break;
 			}
 		}
-		if(romend) break;
+		if (romend)
+			break;
 	}
 	return i;
 }
 
+enum Command
+{
+	COMMAND_HEALTH_CHECK = 0,
+	COMMAND_GET_GAME_SIZE = 1,
+	COMMAND_GET_SAVE_SIZE = 2,
+	COMMAND_READ_DATA = 3,
+	COMMAND_WRITE_SAVE = 4,
+	COMMAND_ECHO = 5,
+};
+
 //---------------------------------------------------------------------------------
 // Program entry point
 //---------------------------------------------------------------------------------
-int main(void) {
-//---------------------------------------------------------------------------------
+int main(void)
+{
+	//---------------------------------------------------------------------------------
 
 	// the vblank interrupt must be enabled for VBlankIntrWait() to work
 	// since the default dispatcher handles the bios flags no vblank handler
@@ -53,189 +129,87 @@ int main(void) {
 
 	consoleDemoInit();
 	REG_JOYTR = 0;
+	xcomms_init();
 	// ansi escape sequence to set print co-ordinates
 	// /x1b[line;columnH
-	u32 i;
 	iprintf("\x1b[9;2HGBA Link Cable Dumper v1.6\n");
 	iprintf("\x1b[10;4HPlease look at the TV\n");
 	// disable this, needs power
 	SNDSTAT = 0;
 	SNDBIAS = 0;
-	// Set up waitstates for EEPROM access etc. 
+	// Set up waitstates for EEPROM access etc.
 	REG_WAITCNT = 0x0317;
-	//clear out previous messages
+	// clear out previous messages
 	REG_HS_CTRL |= JOY_RW;
-	while (1) {
-		if(REG_HS_CTRL&JOY_READ)
+
+	s32 game_size = -1;
+	while (game_size == -1)
+	{
+		game_size = getGameSize();
+	}
+	// For some reason the first time can be incorrect sometimes?
+	game_size = getGameSize();
+
+	u32 save_size = SaveSize(save_data, game_size);
+	iprintf("\x1b[11;4HGetting save data\n");
+	backup_save(save_data, save_size);
+	iprintf("\x1b[11;4HGame: %lx, save: %lx\n", game_size, save_size);
+
+	u32 buf[MAX_BUF_SIZE] = {0};
+
+	while (true)
+	{
+		u32 command = xcomms_recv();
+		if (command == COMMAND_HEALTH_CHECK)
 		{
-			REG_HS_CTRL |= JOY_RW;
-			s32 gamesize = getGameSize();
-			u32 savesize = SaveSize(save_data,gamesize);
-			REG_JOYTR = gamesize;
-			//wait for a cmd receive for safety
-			while((REG_HS_CTRL&JOY_WRITE) == 0) ;
-			REG_HS_CTRL |= JOY_RW;
-			REG_JOYTR = savesize;
-			//wait for a cmd receive for safety
-			while((REG_HS_CTRL&JOY_WRITE) == 0) ;
-			REG_HS_CTRL |= JOY_RW;
-			if(gamesize == -1)
-			{
-				REG_JOYTR = 0;
-				continue; //nothing to read
-			}
-			//game in, send header
-			for(i = 0; i < 0xC0; i+=4)
-			{
-				REG_JOYTR = *(vu32*)(0x08000000+i);
-				while((REG_HS_CTRL&JOY_READ) == 0) ;
-				REG_HS_CTRL |= JOY_RW;
-			}
-			REG_JOYTR = 0;
-			//wait for other side to choose
-			while((REG_HS_CTRL&JOY_WRITE) == 0) ;
-			REG_HS_CTRL |= JOY_RW;
-			u32 choseval = REG_JOYRE;
-			if(choseval == 0)
-			{
-				REG_JOYTR = 0;
-				continue; //nothing to read
-			}
-			else if(choseval == 1)
-			{
-				//disable interrupts
-				u32 prevIrqMask = REG_IME;
-				REG_IME = 0;
-				//dump the game
-				for(i = 0; i < gamesize; i+=4)
-				{
-					REG_JOYTR = *(vu32*)(0x08000000+i);
-					while((REG_HS_CTRL&JOY_READ) == 0) ;
-					REG_HS_CTRL |= JOY_RW;
-				}
-				//restore interrupts
-				REG_IME = prevIrqMask;
-			}
-			else if(choseval == 2)
-			{
-				//disable interrupts
-				u32 prevIrqMask = REG_IME;
-				REG_IME = 0;
-				//backup save
-				switch (savesize){
-				case 0x200:
-					GetSave_EEPROM_512B(save_data);
-					break;
-				case 0x2000:
-					GetSave_EEPROM_8KB(save_data);
-					break;
-				case 0x8000:
-					GetSave_SRAM_32KB(save_data);
-					break;
-				case 0x10000:
-					GetSave_FLASH_64KB(save_data);
-					break;
-				case 0x20000:
-					GetSave_FLASH_128KB(save_data);
-					break;
-				default:
-					break;
-				}
-				//restore interrupts
-				REG_IME = prevIrqMask;
-				//say gc side we read it
-				REG_JOYTR = savesize;
-				//wait for a cmd receive for safety
-				while((REG_HS_CTRL&JOY_WRITE) == 0) ;
-				REG_HS_CTRL |= JOY_RW;
-				//send the save
-				for(i = 0; i < savesize; i+=4)
-				{
-					REG_JOYTR = *(vu32*)(save_data+i);
-					while((REG_HS_CTRL&JOY_READ) == 0) ;
-					REG_HS_CTRL |= JOY_RW;
-				}
-			}
-			else if(choseval == 3 || choseval == 4)
-			{
-				REG_JOYTR = savesize;
-				if(choseval == 3)
-				{
-					//receive the save
-					for(i = 0; i < savesize; i+=4)
-					{
-						while((REG_HS_CTRL&JOY_WRITE) == 0) ;
-						REG_HS_CTRL |= JOY_RW;
-						*(vu32*)(save_data+i) = REG_JOYRE;
-					}
-				}
-				else
-				{
-					//clear the save
-					for(i = 0; i < savesize; i+=4)
-						*(vu32*)(save_data+i) = 0;
-				}
-				//disable interrupts
-				u32 prevIrqMask = REG_IME;
-				REG_IME = 0;
-				//write it
-				switch (savesize){
-				case 0x200:
-					PutSave_EEPROM_512B(save_data);
-					break;
-				case 0x2000:
-					PutSave_EEPROM_8KB(save_data);
-					break;
-				case 0x8000:
-					PutSave_SRAM_32KB(save_data);
-					break;
-				case 0x10000:
-					PutSave_FLASH_64KB(save_data);
-					break;
-				case 0x20000:
-					PutSave_FLASH_128KB(save_data);
-					break;
-				default:
-					break;
-				}
-				//restore interrupts
-				REG_IME = prevIrqMask;
-				//say gc side we're done
-				REG_JOYTR = 0;
-				//wait for a cmd receive for safety
-				while((REG_HS_CTRL&JOY_WRITE) == 0) ;
-				REG_HS_CTRL |= JOY_RW;
-			}
-			REG_JOYTR = 0;
+			xcomms_send(0xc0de);
+			continue;
 		}
-		else if(REG_HS_CTRL&JOY_WRITE)
+
+		u32 size = xcomms_recv();
+		u32 max_size = command == COMMAND_WRITE_SAVE ? MAX_SAVE_SIZE : MAX_BUF_SIZE;
+		size = size >= max_size ? max_size : size;
+		u32 *cmd_buf = command == COMMAND_WRITE_SAVE ? (u32 *)save_data : buf;
+
+		for (u32 i = 0; i < size; i++)
 		{
-			REG_HS_CTRL |= JOY_RW;
-			u32 choseval = REG_JOYRE;
-			if(choseval == 5)
-			{
-				//disable interrupts
-				u32 prevIrqMask = REG_IME;
-				REG_IME = 0;
-				//dump BIOS
-				for (i = 0; i < 0x4000; i+=4)
-				{
-					// the lower bits are inaccurate, so just get it four times :)
-					u32 a = MidiKey2Freq((WaveData *)(i-4), 180-12, 0) * 2;
-					u32 b = MidiKey2Freq((WaveData *)(i-3), 180-12, 0) * 2;
-					u32 c = MidiKey2Freq((WaveData *)(i-2), 180-12, 0) * 2;
-					u32 d = MidiKey2Freq((WaveData *)(i-1), 180-12, 0) * 2;
-					REG_JOYTR = ((a>>24<<24) | (d>>24<<16) | (c>>24<<8) | (b>>24));
-					while((REG_HS_CTRL&JOY_READ) == 0) ;
-					REG_HS_CTRL |= JOY_RW;
-				}
-				//restore interrupts
-				REG_IME = prevIrqMask;
-			}
-			REG_JOYTR = 0;
+			cmd_buf[i] = xcomms_recv();
 		}
-		Halt();
+
+		switch (command)
+		{
+		case COMMAND_GET_GAME_SIZE:
+			xcomms_send(game_size);
+			break;
+		case COMMAND_GET_SAVE_SIZE:
+			xcomms_send(save_size);
+			break;
+		case COMMAND_READ_DATA:
+			u32 *addr = (u32 *)cmd_buf[0];
+			if (addr == (u32 *)0x7000000)
+			{
+				addr = (u32 *)save_data;
+			}
+			for (u32 i = 0; i < cmd_buf[1]; i++)
+			{
+				xcomms_send(addr[i]);
+			}
+			break;
+		case COMMAND_WRITE_SAVE:
+			if ((size * 4) == save_size)
+			{
+				write_save((u8 *)cmd_buf, save_size);
+			}
+			break;
+		case COMMAND_ECHO:
+			for (u32 i = 0; i < size; i++)
+			{
+				xcomms_send(cmd_buf[i]);
+			}
+			break;
+		default:
+			xcomms_send(0xbad);
+			break;
+		}
 	}
 }
-
-
